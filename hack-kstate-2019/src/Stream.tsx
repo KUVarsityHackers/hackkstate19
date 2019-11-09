612f3775d2869a8d300c5b6c12a77224a3eff438
John Quitno <john.quitno@ku.edu>
2019-11-09 11:47:58 -0600

    Streaming

---
+++ hack-kstate-2019/src/Stream.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import React from 'react';
+
+function Stream (props: any) {
+
+const useScript = url => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = url;
+    script.async = false;
+    script.setAttribute("arg", props.session);
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [url]);
+};
+
+
+
+return (
+    <body>
+        <div>
+
+            <div id="scriptId">
+                {useScript("https://meet.jit.si/external_api.js")}
+                {useScript("/JitsiScript.js")}
+            </div>
+        </div>
+    </body>
+    );
+}
+
+export default Stream;
