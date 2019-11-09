0a5942d0a48e0c6150752bb1198560b4cf2eb5e1
czfloyd <floyd.c23@gmail.com>
2019-11-08 21:52:05 -0600

    Switched to ts and added pages

---
+++ hack-kstate-2019/src/EarnScreen/EarnScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import PageEnum from '../PageEnum';
+
+function EarnScreen (props: any) {
+    return (
+        <div className="Splash">
+            <header className="App-header">
+                <p>Earn</p>
+            </header>
+        </div>
+    );
+}
+
+export default EarnScreen;
---
+++ hack-kstate-2019/src/LearnScreen/LearnScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import PageEnum from '../PageEnum';
+
+function LearnScreen (props: any) {
+    return (
+        <div className="Splash">
+            <header className="App-header">
+                <p>Learn</p>
+            </header>
+        </div>
+    );
+}
+
+export default LearnScreen;
