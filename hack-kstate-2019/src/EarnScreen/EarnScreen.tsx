c26850d985db3783b1dc98c131e425a33ec9f1d1
John Quitno <john.quitno@ku.edu>
2019-11-09 02:14:12 -0600

    Earning

--- hack-kstate-2019/src/EarnScreen/EarnScreen.tsx
+++ hack-kstate-2019/src/EarnScreen/EarnScreen.tsx
@@ -1,15 +1,69 @@
 import React from 'react';
 import '../App.css';
 import PageEnum from '../PageEnum';
+import useForm from "react-hook-form";
+import { ISession } from 'src/types';
+import { string } from 'prop-types';
+
 
 function EarnScreen (props: any) {
+    let sessionId
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit =  async ( {name, publickey, title, subject, price}:any) => {
+        const convertedValues: ISession = { instructor: {
+                                                        name,
+                                                        id: null,
+                                                        publickey
+                                                    },
+                                            title,
+                                            subject,
+                                            price};
+        console.log(convertedValues);
+        const url = 'session';
+        const data = convertedValues;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(data), // data can be `string` or {object}!
+                headers: {
+                'Content-Type': 'application/json'
+                }
+            });
+        } catch (error) {
+            console.error('Error:', error);
+          }
+    }
+
     return (
         <div className="Splash">
             <header className="App-header">
                 <p>Earn</p>
             </header>
+            <body>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>First name</label><br/>
+                    <input className="tutorForm" name="name" ref={register}></input><br/>
+                    <label>Title</label><br/>
+                    <input className="tutorForm" name="title" ref={register}></input><br/>
+                    <label>Subject</label><br/>
+                    <input className="tutorForm" name="subject" ref={register}></input><br/>
+                    <label>Price</label><br/>
+                    <input className="tutorForm" name="price" ref={register}></input><br/>
+                    <label>Public Key</label><br/>
+                    <input className="tutorForm" name="publickey" ref={register}></input><br/>
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
         </div>
     );
 }
-
+interface formSubmit {
+    "name": string;
+    "title": string;
+    "subject": string;
+    "price": string;
+    "publickey": string;
+}
 export default EarnScreen;
--- hack-kstate-2019/src/HomeScreen/HomeScreen.tsx
+++ hack-kstate-2019/src/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../App.css';
 import PageEnum from '../PageEnum';
+import homePageButton from '../Style/buttonStyle';
 
 function HomeScreen (props: any) {
     return (
         <div className="Splash">
+
             <header className="App-header">
-                <p>Home</p>
+
+                <p>Name</p>
             </header>
+            <body className="App-Body">
+                <button style={homePageButton} onClick={() => props.onPageChange(PageEnum.LEARN)}> Learn </button>
+                <button style={homePageButton} onClick={() => props.onPageChange(PageEnum.EARN)}> Earn </button>               
+            </body>
+
         </div>
     );
 }
