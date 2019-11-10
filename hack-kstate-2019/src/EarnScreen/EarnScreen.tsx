7bc76634f6af4e5e990d132501b1e835effcca78
Nathan Nichols <n545n545@guest-wireless-221-215.campus.ksu.edu>
2019-11-09 21:55:10 -0600

    update branch

--- hack-kstate-2019/src/EarnScreen/EarnScreen.tsx
+++ hack-kstate-2019/src/EarnScreen/EarnScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
 import PageEnum from '../PageEnum';
 import useForm from "react-hook-form";
@@ -6,6 +6,7 @@ import { ISession } from 'src/types';
 import { string } from 'prop-types';
 import Stream from '../Stream'
 import EarnStream from './EarnStream';
+import QrReader from 'react-qr-reader'
 
 let sessionId:any;
 let addressId:any;
@@ -42,11 +43,55 @@ function EarnScreen (props: any) {
           }
     }
 
+    // let state = {
+    //       result: 'No result'
+    //     }
+      
+    // let handleScan = (data) => {
+    //       if (data) {
+    //         state = {
+    //           result: data
+    //         };
+    //       }
+    //     };
+    // let handleError = err => {
+    //       console.error(err)
+    //     };
+
+    class Test extends Component {
+        state = {
+          result: 'No result'
+        }
+      
+        handleScan = data => {
+          if (data) {
+            this.setState({
+              result: data
+            })
+          }
+        }
+        handleError = err => {
+          console.error(err)
+        }
+    }
+    
+    let QR = new Test('No result');
+
     if(sessionId) {
         return (<EarnStream streamId={sessionId} address={addressId}/>)
     } else {
         return (
             <div id="Form">
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                <QrReader
+                    delay={300}
+                    onError={QR.handleError}
+                    onScan={QR.handleScan}
+                    style={{ width: '50%' }}
+                />
+                </div>
+              <p>Scan QR Code For Wallet Address</p>
+                
                 <body>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <label className="formNames">First name</label><br/>
